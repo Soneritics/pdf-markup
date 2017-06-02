ed4ab97a5f935d595851398bc1c1f05036544084
Jordi Jolink <info@soneritics.nl>
2017-06-02 23:10:04 +0200

    Add tool window properties for the Text plugin

--- Markup/Plugins/Text.js
+++ Markup/Plugins/Text.js
@@ -4,15 +4,24 @@ var MarkupText = {
     },
 
     Paint: function(elementObject) {
-        $('#' + elementObject.Handle).text(elementObject.text);
+        $('#' + elementObject.Handle)
+            .text(elementObject.text)
+            .css({
+                whiteSpace: 'pre-wrap',
+                left: SizeCalculator.ToPixels(elementObject.x) + 'px',
+                top: SizeCalculator.ToPixels(elementObject.y) + 'px',
+                color: elementObject.color,
+                fontSize: elementObject.size + 'px',
+                fontWeight: elementObject.bold ? 'bold' : 'normal'
+            });
     },
 
     Activate: function(elementObject) {
-        
+
     },
 
     Update: function(elementObject) {
-        
+        this.Paint(elementObject);
     },
 
     New: function(markup, text) {
@@ -20,7 +29,55 @@ var MarkupText = {
            type: "text",
            text: text ? text : "New textfield",
            x: 10,
-           y: 10
+           y: 10,
+           color: '#00000',
+           size: 12,
+           bold: false,
+           properties: {
+               text: {
+                   label: 'Text',
+                   component: 'textarea'
+               },
+               x: {
+                   label: 'Left (mm)',
+                   component: 'number'
+               },
+               y: {
+                   label: 'Top (mm)',
+                   component: 'number'
+               },
+               color: {
+                   label: 'Color',
+                   component: 'select',
+                   items: {
+                       'Black': '#00000',
+                       'Silver': '#C0C0C0',
+                   }
+               },
+               size: {
+                   label: 'Size',
+                   component: 'select',
+                   items: {
+                       '8': '8',
+                       '9': '9',
+                       '10': '10',
+                       '11': '11',
+                       '12': '12',
+                       '13': '13',
+                       '14': '14',
+                       '15': '15',
+                       '16': '16',
+                       '17': '17',
+                       '18': '18',
+                       '19': '19',
+                       '20': '20'
+                   }
+               },
+               bold: {
+                   'label': 'Bold',
+                   component: 'boolean'
+               }
+           }
        });
     }
 };
