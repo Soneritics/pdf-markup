80506215a0693194d2cdf337ba2530250d76debb
Jordi Jolink <info@soneritics.nl>
2017-06-03 22:19:25 +0200

    Implement Image plugin functionality

--- Markup/Plugins/Image.js
+++ Markup/Plugins/Image.js
@@ -4,7 +4,36 @@ var MarkupImage = {
     },
 
     Paint: function(elementObject) {
-        $('#' + elementObject.Handle).text('image');
+        var fullyPainted = false;
+
+        if (elementObject.source == null) {
+            $('#' + elementObject.Handle).text('Please provide a source for the image');
+        } else {
+            $('#' + elementObject.Handle).html('<img>');
+            var img = $('#' + elementObject.Handle).children('img')
+
+            if (elementObject.width == null) {
+                $(img).on('load', function() {
+                    elementObject.width = Math.round(SizeCalculator.ToMM($(this).width()));
+                    MarkupImage.Paint(elementObject);
+                    $(this).trigger('painted', elementObject);
+                });
+            } else {
+                $(img).css('width', SizeCalculator.ToPixels(elementObject.width));
+                fullyPainted = true;
+            }
+
+            $(img)
+                .attr('src', elementObject.source)
+                .css({
+                    left: SizeCalculator.ToPixels(elementObject.x) + 'px',
+                    top: SizeCalculator.ToPixels(elementObject.y) + 'px'
+                });
+        }
+
+        if (fullyPainted) {
+            $(this).trigger('painted', elementObject);
+        }
     },
 
     Update: function(elementObject) {
@@ -16,7 +45,30 @@ var MarkupImage = {
            type: "image",
            source: null,
            x: 10,
-           y: 10
+           y: 10,
+           width: null,
+           properties: {
+               source: {
+                   label: 'Source',
+                   component: 'browse'
+               },
+               x: {
+                   label: 'Left (mm)',
+                   component: 'number'
+               },
+               y: {
+                   label: 'Top (mm)',
+                   component: 'number'
+               },
+               width: {
+                   label: 'Width (mm)',
+                   component: 'range',
+                   range: {
+                       from: 1,
+                       to: formats[settings.format][0]
+                   }
+               }
+           }
        });
     }
 };
