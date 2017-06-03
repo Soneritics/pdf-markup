8659a8454bc090c2318a791b2dd1eca2ee5c6386
Jordi Jolink <info@soneritics.nl>
2017-06-03 10:59:42 +0200

    Show the JSON output by registering on the events

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ var markup = null;
 $(function() {
    markup = Markup('#pdf-markup', MarkupTools('.button-bar > div'));
 
+   $(markup).on('update add', function() {
+       $('#result').text(JSON.stringify(markup.Serialize()));
+    });
+
    $('.button-bar > ul').find('a').each(function() {
         var pluginName = $(this).data('plugin');
         var plugin = window[pluginName];
